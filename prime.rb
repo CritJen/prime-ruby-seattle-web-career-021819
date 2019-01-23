19595a4993152105d7193fc1c85bb996ba2a7a0e
Jennifer Clark <jen.boyd.clark@gmail.com>
2019-01-23 22:46:06 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 def prime? (number)
-  counter = number.abs -1
+  counter = number -1
   while counter > 1
   if number == 2 || number == 3
     true
-  else number.abs % counter == 0
+  else number % counter == 0
       false
     end
   counter -= 1
